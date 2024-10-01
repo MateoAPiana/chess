e859a84872c0cc354ba4f185dc035fe38205c3ed
M. Andrés Piana <mateoapiana@gmail.com>
2024-10-01 12:24:01 -0300

    let select the piece to move

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react"
 import { useGameStore } from "../store/game"
 
 export function Board() {
   const board = useGameStore(state => state.board)
+  const [pieceToMove, setPieceToMove] = useState("")
   return (
     <main className="board">
       {
-        board.map((r, index) => {
+        board.map((r, indexRow) => {
           return (
-            <div key={index.toString()} className="row">
+            <div key={indexRow.toString()} className="row">
               {
                 r.map((c, index) => {
                   return (
-                    <div key={index.toString()} className="cell">
+                    <div
+                      key={index.toString()}
+                      className="cell"
+                      id={`${indexRow}${index}`}
+                      onMouseDown={() => setPieceToMove(`${indexRow}${index}`)}
+                      style={{ backgroundColor: pieceToMove === `${indexRow}${index}` ? "#f334" : "transparent" }}
+                    >
                       {c.piece !== "" && <img className="piece" src={`../../public/${c.color}/${c.piece}.png`} alt="piece" />}
                     </div>
                   )
