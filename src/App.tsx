16d1a5e60dc8da0dc5af7472161660164d41867b
M. Andrés Piana <mateoapiana@gmail.com>
2024-10-03 16:50:06 -0300

    get the enemy moves

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import './App.css'
 import { Game } from './components/game'
+import { io } from "socket.io-client"
+import { useGameStore } from './store/game'
+
+const socket = io("http://localhost:3000")
+
+export function sendMoves({
+  from,
+  to,
+}: {
+  from: [number, number]
+  to: [number, number]
+}) {
+  socket.emit("move", { fromEnemy: from, toEnemy: to })
+}
 
 function App() {
+  const getEnemyMoves = useGameStore(state => state.getEnemyMoves)
+  socket.on(
+    "getMove",
+    async ({
+      from,
+      to,
+    }: {
+      from: [number, number]
+      to: [number, number]
+    }) => {
+      getEnemyMoves({ from, to })
+      return { from, to }
+    },
+  )
   return (
     <div className='App'>
       <Game />
--- src/store/game.ts
+++ src/store/game.ts
@@ -2,8 +2,13 @@ import { create } from "zustand"
 import { devtools, persist } from "zustand/middleware"
 import type { Board } from "../../types.d"
 import { initialBoard } from "../../constants"
+import { sendMoves } from "../App"
 
 interface State {
+	getEnemyMoves: ({
+		from,
+		to,
+	}: { from: [number, number]; to: [number, number] }) => void
 	board: Board
 	move: (from: [number, number], to: [number, number]) => void
 }
@@ -13,7 +18,25 @@ export const useGameStore = create<State>()(
 		(set, get) => {
 			return {
 				board: initialBoard,
+				getEnemyMoves({ from, to }) {
+					console.log({ from, to })
+					const board = get().board
+					const newBoard: Board = [
+						board[0],
+						board[1],
+						board[2],
+						board[3],
+						board[4],
+						board[5],
+						board[6],
+						board[7],
+					]
+					newBoard[to[0]][to[1]] = board[from[0]][from[1]]
+					newBoard[from[0]][from[1]] = { piece: "", color: "" }
+					set({ board: newBoard })
+				},
 				move: (from, to) => {
+					sendMoves({ from, to })
 					const board = get().board
 					board[to[0]][to[1]] = board[from[0]][from[1]]
 					board[from[0]][from[1]] = { piece: "", color: "" }
