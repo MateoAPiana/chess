58ba2676cc35861ee634ff61c61d329023c78bf1
M. Andrés Piana <mateoapiana@gmail.com>
2024-10-12 16:40:16 -0300

    The pawn can to eat

--- src/services/calcMove.ts
+++ src/services/calcMove.ts
@@ -24,24 +24,18 @@ export function calcMove(
 	switch (piece) {
 		case "king":
 			if (from[0] === to[0]) {
-				if (from[1] - 1 === to[1] || from[1] + 1 === to[1]) {
-					return true
-				}
+				if (from[1] - 1 === to[1] || from[1] + 1 === to[1]) return true
 			} else if (from[0] - 1 === to[0] || from[0] + 1 === to[0]) {
 				if (
 					from[1] - 1 === to[1] ||
 					from[1] + 1 === to[1] ||
 					from[1] === to[1]
-				) {
-					return true
-				}
+				) return true
 			}
 			return
 		case "pawn":
 			if (from[0] === 6 && to[0] === 4 && from[1] === to[1]) return true
-			if (from[0] - 1 === to[0] && from[1] === to[1]) {
-				return true
-			}
+			if (from[0] - 1 === to[0] && (from[1] === to[1] || from[1] - 1 === to[1] || from[1] + 1 === to[1])) return true
 			return
 		case "knight":
 			if (
