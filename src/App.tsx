b7a36da1224ac68c0929907c9a7c40b67c86b9ff
M. Andrés Piana <153022165+MateoAPiana@users.noreply.github.com>
2024-11-21 10:36:38 -0300

    fix the routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import HomePage from './components/Home'
 import Game from './components/game'
 import { Routes, Route } from 'react-router-dom'
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<Game />} />
+        <Route path='/' element={<HomePage />} />
         <Route path='/game' element={<Game />} />
       </Routes>
     </div>
